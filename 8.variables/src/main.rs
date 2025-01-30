7f2bd377a0dc375d077f2d0f4070af18db55ffc6
amkumar072 <amkumar072@gmail.com>
2025-01-30 15:00:38 +0000

    variables

---
+++ 8.variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+
+    // immuttable variable
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    // muttable variable
+    let mut y = 10;
+    println!("The value of y is: {}", y);
+    y = 15;
+    println!("The value of y is: {}", y);
+}

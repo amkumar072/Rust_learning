a7c08a0da7ba584348e0d5fb49d563e9fca9055c
amkumar072 <amkumar072@gmail.com>
2025-01-30 14:47:36 +0000

    struct

--- 7.struct/src/main.rs
+++ 7.struct/src/main.rs
@@ -1,3 +1,34 @@
 // struct
 
-fn main() {}
+fn main() {
+    let mut account = BankAccount {
+        owner: "Alice".to_string(),
+        balance: 100.0,
+    };
+
+    account.check_balance();
+
+    account.withdraw(50.05);
+
+    account.check_balance();
+}
+
+struct BankAccount {
+    owner: String,
+    balance: f64,
+}
+
+impl BankAccount {
+    // muttable reference
+    fn withdraw(&mut self, amount: f64) {
+        println!("Withdrawing ${} from account of {}", amount, self.owner);
+        self.balance -= amount;
+    }
+    // immutable reference
+    fn check_balance(&self) {
+        println!(
+            "The balance of account of {} is ${}",
+            self.owner, self.balance
+        );
+    }
+}
